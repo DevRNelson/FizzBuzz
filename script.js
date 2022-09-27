07785b667b040ab678e65a5a7e61d6b31d63baf5
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2022-09-27 10:23:51 -0400

    Added algorithm for FizzBuzz css and instructions

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
- let answer = parseInt(prompt("Please Input the number you would like to FizzBuzz up to!"));
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
 
- for (let i = i = 1; i <= answer; i++) {
-     console.log(i);
- }
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
